2089114c939eb2d2030667823fd112bdeca2b602
23clhelle <chellebusch2@unl.edu>
2024-10-16 18:08:25 -0500

    muchhh better

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,27 +13,25 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-      <div className="homepage-title">
-        <div className="me">
-          <Heading as="h1" className="hero__title">
-            {siteConfig.title}
-          </Heading>
-        
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className="homepage-title">
+          <div className="me">
+            <Heading as="h1" className="hero__title">
+              {siteConfig.title}
+            </Heading>
+          
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to="/blog"
+              >
+                Blog Posts
+              </Link>
+            </div>
+          </div>
+          <img className="homepage-img" src={require('@site/static/img/UI_Headshot.jpg').default} alt="Headshot" />
         </div>
-
-
-        <img className="homepage-img" src={require('@site/static/img/UI_Headshot.jpg').default} alt="Headshot" />
-        </div>
-
-        <div className={styles.buttons}></div>
-        <br></br>
-        <Link
-          className="button button--secondary button--lg"
-          to="/blog"
-        >
-          Blog Posts
-        </Link>
       </div>
     </header>
   );
